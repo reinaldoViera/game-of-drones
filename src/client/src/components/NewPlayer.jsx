b788de93cfc174c66c088085b41c8b46ab750599
Reinaldo Viera <rviera@redgage.com>
2019-02-15 19:25:35 -0500

    Using some hooks

--- src/client/src/components/NewPlayer.jsx
+++ src/client/src/components/NewPlayer.jsx
@@ -1,65 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@material-ui/core';
 
-export default class NewPlayer extends Component {
-    state = {
-        name: '',
-        valid: false
-    }
-    handleClose = () => {
-        this.setState({
-            name: ''
-        });
-        this.props.handleClose(this.state.name);
-    }
-    handleCancel = () => {
-        this.setState({
-            name: ''
-        });
-        this.props.handleClose();
-    }
-    handleChange = (ev) => {
+export default function NewPlayer({ open, checkName, handleClose }) {
+    const [name, setName] = useState("");
+    const [valid, setValid] = useState(false);
+
+    function handleChange(ev) {
         const name = ev.target.value;
-        let valid = false;
-        if(name && this.props.checkName(name)){
-            valid = true;
-        }
-        this.setState({
-            name,
-            valid
-        });
+        setValid(name && checkName(name));
+        setName(name)
     }
-    render() {
-        const { open } = this.props;
-        const { name, valid } = this.state;
-        return (
-            <Dialog
-                open={open}
-                onClose={this.handleCancel}
-                id="new-player-dialog"
-                aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Add new Player</DialogTitle>
-                <DialogContent>
-                    <TextField
-                        autoFocus
-                        value={name}
-                        onChange={this.handleChange}
-                        margin="dense"
-                        id="new-player-name"
-                        label="Name"
-                        type="text"
-                        fullWidth
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={this.handleCancel} color="secondary">
-                        Cancel
-            </Button>
-                    <Button id="new-player-btn" onClick={this.handleClose} color="primary" disabled={!valid}>
-                        Add
-            </Button>
-                </DialogActions>
-            </Dialog>
-        )
+
+    function handleCancel() {
+        setName("");
+        handleClose();
     }
+
+    function handleAdd() {
+        setName("");
+        handleClose(name);
+    }
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleCancel}
+            id="new-player-dialog"
+            aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Add new Player</DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    value={name}
+                    onChange={handleChange}
+                    margin="dense"
+                    id="new-player-name"
+                    label="Name"
+                    type="text"
+                    fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleCancel} color="secondary">
+                    Cancel
+        </Button>
+                <Button id="new-player-btn" onClick={handleAdd} color="primary" disabled={!valid}>
+                    Add
+        </Button>
+            </DialogActions>
+        </Dialog>
+    )
 }
+
+
