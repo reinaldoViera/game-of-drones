55e2131c2d9a3a3129dd6150093c1717534034c7
Reinaldo Viera <rviera@redgage.com>
2019-03-20 10:22:07 -0400

    Lazy loading with Suspense and React.lazy

--- src/client/src/pages/Routes.jsx
+++ src/client/src/pages/Routes.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route } from "react-router-dom";
 import StartGame from './StartGame';
-import Round from './Round';
+const Round = React.lazy(() => import("./Round"));
+
+function createWrapper(LazyCmp) {
+  return (props) => (
+    <Suspense
+      fallback={<div>Loading...</div>}>
+      <LazyCmp {...props}/>
+    </Suspense>
+  )
+}
+const RoundWrapper = createWrapper(Round);
 
 export default function Routes() {
   return (
     <div>
-        <Route exact path='/' component={StartGame}/>
-        <Route exact path='/round' component={Round}/>
+      <Route exact path='/' component={StartGame} />
+      <Route exact path='/round' component={RoundWrapper} />
     </div>
   )
 }
